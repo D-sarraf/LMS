25ec9e7a3321e6593f421d81138be0c34d611b68
D-sarraf <artdivyakumari@gmail.com>
2026-02-03 20:39:54 +0530

    Add files via upload

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace LMS.Models
+{
+    public class Category
+    {
+       
+        public int? CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
